2859e9f7d9016ee8988b403134e3de3ff41226c8
Kasper Kain <kasperkain@gmail.com>
2026-02-28 17:48:36 -0500

    init release

--- world/map_loader.py
+++
@@ -1,6 +0,0 @@
-from utils.paths import resource_path
-
-
-def load_map(path: str):
-    with open(resource_path(path), "r", encoding="utf-8") as f:
-        return [list(line.rstrip("\n")) for line in f]
